4321dcd12b5bc607ea25356eff2439dc06d324a5
vv7x-x <cvcvccvc48@gmail.com>
2026-07-04 17:17:21 +0300

    Update Supabase URL and anon key in config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
-export const SUPABASE_URL = 'https://hkauyitbauycembyjloj.supabase.co';
-export const SUPABASE_ANON_KEY = 'sb_publishable_06ZKgeqC7-47FF7Ef48Q5Q_Zr9l0BU6';
+export const SUPABASE_URL = 'https://blgmnndiigabitnxgsij.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_dHTnSjQN20_CpV0UJ1Chiw_a8XGTHbV';
 export const APP_NAME = 'GYMOS';
 export const STORAGE_BUCKET = 'member-photos';
 export const APP_VERSION = '2.0.0';
